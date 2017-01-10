8dababda83b13867cc5128d7ee3f26f9eeb8811d
Kyle Aleshire <kjaleshire@gmail.com>
2017-01-10 11:14:32 -0600

    Update to Swift 3

--- RustOwnershipPassing/AppDelegate.swift
+++ RustOwnershipPassing/AppDelegate.swift
@@ -8,36 +8,109 @@
 
 import UIKit
 
+extension RustByteSlice {
+    func asUnsafeBufferPointer() -> UnsafeBufferPointer<UInt8> {
+        return UnsafeBufferPointer(start: bytes, count: len)
+    }
+    
+    func asString(_ encoding: String.Encoding = String.Encoding.utf8) -> String? {
+        return String(bytes: asUnsafeBufferPointer(), encoding: encoding)
+    }
+}
+
+class RustNamedData {
+    fileprivate let raw: OpaquePointer
+    
+    init() {
+        raw = named_data_new()
+    }
+    
+    deinit {
+        named_data_destroy(raw)
+    }
+    
+    var name: String {
+        let byteSlice = named_data_get_name(raw)
+        return byteSlice.asString()!
+    }
+    
+    var count: Int {
+        return named_data_count(raw)
+    }
+}
+
+class SwiftObject {
+    deinit {
+        print("SwiftObject is being deallocated")
+    }
+    
+    fileprivate func callbackWithArg(_ arg: Int) {
+        print("SwiftObject: received callback with arg \(arg)")
+    }
+    
+    func sendToRust() {
+        let ownedPointer = UnsafeMutableRawPointer(Unmanaged.passRetained(self).toOpaque())
+        let wrapper = swift_object(
+            user: ownedPointer,
+            destroy: destroy,
+            callback_with_int_arg: callback_with_int_arg
+        )
+        give_object_to_rust(wrapper)
+    }
+}
+
+private func callback_with_int_arg(_ user: UnsafeMutableRawPointer?, arg: Int32) {
+    if let ptr = user {
+        let obj: SwiftObject = Unmanaged.fromOpaque(ptr).takeUnretainedValue()
+        obj.callbackWithArg(Int(arg))
+    }
+}
+
+private func destroy(_ user: UnsafeMutableRawPointer?) {
+    if let ptr = user {
+        let _ = Unmanaged<SwiftObject>.fromOpaque(ptr).takeRetainedValue()
+    }
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        
+        let namedData = RustNamedData()
+        
+        print("namedData.name = \(namedData.name)")
+        print("namedData.count = \(namedData.count)")
+        
+        let obj = SwiftObject()
+        obj.sendToRust()
+        
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
+    func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
-    func applicationDidEnterBackground(application: UIApplication) {
+    func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
-    func applicationWillEnterForeground(application: UIApplication) {
+    func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
+    func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
-    func applicationWillTerminate(application: UIApplication) {
+    func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
---
+++ RustOwnershipPassing/RustOwnershipPassing-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  RustOwnershipPassing-Bridging-Header.h
+//  RustOwnershipPassing
+//
+//  Created by Kyle J Aleshire on 2/15/16.
+//  Copyright © 2016 Kyle J Aleshire. All rights reserved.
+//
+
+#ifndef RustOwnershipPassing_Bridging_Header_h
+#define RustOwnershipPassing_Bridging_Header_h
+
+#import "ownership_passing.h"
+
+#endif /* RustOwnershipPassing_Bridging_Header_h */
